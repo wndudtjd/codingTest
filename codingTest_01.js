71d8f38f12535410e47c4643760398e748917f29
wndudtjd <dudtjdwn1575@naver.com>
2023-02-14 00:21:53 +0900

    항해99 lv.1 코딩테스트

---
+++ codingTest_01.js
@@ -0,0 +1,45 @@
+// 명함 지갑을 만드는 회사에서 지갑의 크기를 정하려고 합니다. 다양한 모양과 크기의 명함들을 모두 수납할 수 있으면서, 작아서 들고 다니기 편한 지갑을 만들어야 합니다. 이러한 요건을 만족하는 지갑을 만들기 위해 디자인팀은 모든 명함의 가로 길이와 세로 길이를 조사했습니다.
+// 아래 표는 4가지 명함의 가로 길이와 세로 길이를 나타냅니다.
+
+// 가장 긴 가로 길이와 세로 길이가 각각 80, 70이기 때문에 80(가로) x 70(세로) 크기의 지갑을 만들면 모든 명함들을 수납할 수 있습니다. 하지만 2번 명함을 가로로 눕혀 수납한다면 80(가로) x 50(세로) 크기의 지갑으로 모든 명함들을 수납할 수 있습니다. 이때의 지갑 크기는 4000(=80 x 50)입니다.
+// 모든 명함의 가로 길이와 세로 길이를 나타내는 2차원 배열 sizes가 매개변수로 주어집니다. 모든 명함을 수납할 수 있는 가장 작은 지갑을 만들 때, 지갑의 크기를 return 하도록 solution 함수를 완성해주세요.
+
+// 내가짠코드
+function solution01(sizes) {
+  var biggerSizeMax = 0;
+  var smallerSizeMax = 0;
+
+  for (let i = 0; i < sizes.length; i++) {
+    if (Math.max(sizes[i][0], sizes[i][1]) > biggerSizeMax) {
+      biggerSizeMax = Math.max(sizes[i][0], sizes[i][1])
+    }
+    if (Math.min(sizes[i][0], sizes[i][1]) > smallerSizeMax) {
+      smallerSizeMax = Math.min(sizes[i][0], size[i][1])
+    }
+  }
+  return biggerSizeMax * smallerSizeMax;
+}
+
+// 항해99 백승호님 코드
+function solution02(sizes) {
+  var biggerSizeMax = 0;
+  var biggerSizeMax = 0;
+
+  for (let i = 0; i < sizes.length; i++) {
+    if (sizes[i][0] > sizes[i][1]) {
+      if (sizes[i][0] >= biggerSizeMax) {
+        biggerSizeMax = sizes[i][0]
+      }
+      if (sizes[i][1] >= smallerSizeMax) {
+        biggerSizeMax = sizes[i][1]
+      }
+    }else {
+      if (sizes[i][1] >= biggerSizeMax) {
+        biggerSizeMax = sizes[i][1]
+      }
+      if (sizes[i][0] >= smallerSizeMax) {
+        biggerSizeMax = sizes[i][0]
+      }
+    }
+  }
+}
